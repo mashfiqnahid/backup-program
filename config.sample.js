f3253fb6336fb422a144236c2e0b500fd50d1267
Mashfiqur Rahman <mashfiqnahid@gmail.com>
2025-04-15 03:06:13 +0600

    sample config file added

---
+++ config.sample.js
@@ -0,0 +1,20 @@
+// config.sample.js
+const user = 'your-username-here';
+const pass = 'your-password-here';
+const CLIENT_ID = 'your-client-id-here';
+const CLIENT_SECRET = 'your-client-secret-here';
+const REFRESH_TOKEN = 'your-refresh-token-here';
+const FILE_PATH = 'your-file-path-here';
+const FILE_NAME = 'your-file-name-here';
+const G_FOLDER_ID = 'your-google-folder-id-here';
+
+module.exports = {
+  user,
+  pass,
+  CLIENT_ID,
+  CLIENT_SECRET,
+  REFRESH_TOKEN,
+  FILE_PATH,
+  FILE_NAME,
+  G_FOLDER_ID
+};
