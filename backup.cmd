5617233a78c0bf8d36495155421ace2b7fc643de
MD. Mashfiqur Rahman <mashfiqnahid!gmail.com>
2025-04-15 14:55:38 +0600

    Backup Script Updated

--- backup.cmd
+++ backup.cmd
@@ -1,1 +1,9 @@
-node .
+@echo off
+set DIR_PATH=%~dp0
+
+echo Task started at %DATE% %TIME% > "%DIR_PATH%\logfile.txt"
+cd /d "%DIR_PATH%" >> "%DIR_PATH%\logfile.txt" 2>&1
+@REM nvm use 23.11.0 >> "%DIR_PATH%\logfile.txt" 2>&1
+node . >> "%DIR_PATH%\logfile.txt" 2>&1
+echo Task completed at %DATE% %TIME% >> "%DIR_PATH%\logfile.txt" 2>&1
+
